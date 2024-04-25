76981e7585c06d5ae3c5a10aa8d0a9298c1efe81
이효재 <orange1th@gmail.com>
2024-04-25 13:00:28 +0900

    feat: 키워드 시각화

--- BE-Python/text_keyword.py
+++ BE-Python/text_keyword.py
@@ -1,5 +1,7 @@
 from krwordrank.word import summarize_with_keywords
 import re
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 print("호출: text_keyword.py")
 
@@ -11,10 +13,21 @@ def split_sentences(text):
 
 def get_keyword(texts):
     try:
-        print(texts)
         keywords = summarize_with_keywords(texts, min_count=1, max_length=10,
             beta=0.85, max_iter=10, stopwords=None, verbose=True)  #min_count로 민감도 조절, 20보다 늘리면 에러
         print(keywords)
+        krwordrank_cloud = WordCloud(
+            font_path = "malgunbd.ttf",
+            width = 800,
+            height = 800,
+            background_color="white"
+        )
+        krwordrank_cloud = krwordrank_cloud.generate_from_frequencies(keywords)
+        fig = plt.figure(figsize=(10, 10))
+        plt.imshow(krwordrank_cloud, interpolation="bilinear")
+        plt.axis('off')  # 눈금과 숫자(축 레이블) 숨김
+        plt.show()
+        # fig.savefig('image.png')
         return texts
     except Exception as e:
         print ({str(e)})

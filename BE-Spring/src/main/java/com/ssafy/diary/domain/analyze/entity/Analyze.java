e59dde0675ac3db34566339083a8b36995c6d8d1
이효재 <orange1th@gmail.com>
2024-04-26 13:50:40 +0900

    fix: Analzye 엔티티

--- BE-Spring/src/main/java/com/ssafy/diary/domain/analyze/entity/Analyze.java
+++ BE-Spring/src/main/java/com/ssafy/diary/domain/analyze/entity/Analyze.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Document(collection = "analyze")
@@ -16,5 +17,6 @@ public class Analyze {
     @Id
     @Field(name="diary_index")
     private Long diaryIndex;
-    private List<String> emotion;
+    private HashMap<String,Double[]> emotion;
+    private HashMap<String,Double> keyword;
 }

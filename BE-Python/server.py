46e354ffadb6d00ba599376418f2c7d153e8790b
이효재 <orange1th@gmail.com>
2024-05-02 14:50:22 +0900

    feat: 스웨거 자체 구현, Post Body

--- BE-Python/server.py
+++ BE-Python/server.py
@@ -2,9 +2,12 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 import text_emotion
 import text_keyword
 import mongo_util
+from pydantic import BaseModel
 
 print("호출: server.py")
 
@@ -12,6 +15,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 origins = ["*"]
 
@@ -31,6 +36,11 @@ mongo_collection=mongo_db['analyze']
 @app.get("/")
 def read_root(): 
     return {"Hello": "World"}
+
+@app.get("/custom/docs", include_in_schema=False)
+async def custom_swagger_ui():
+    html_content = open('static/swagger.html', 'r').read()
+    return HTMLResponse(content=html_content)
 
 @app.post("/api/ai/emotion")
 def analyze_text(diary_index:int, diary_content:str):
@@ -53,20 +63,20 @@ def get_keyword(diary_index:int, diary_content:str):
     except Exception as e:
         return {str(e)}
     
-@app.post("/api/ai/analyze") #메인 기능
-async def analyze_diary(request: Request):
-    try:
-        body = await request.json()
-        diary_index = body['diary_index']
-        diary_content = body['diary_content']
+class DiaryEntry(BaseModel):
+    diary_index: int
+    diary_content: str
     
+@app.post("/api/ai/analyze") #메인 기능
+async def analyze_diary(entry: DiaryEntry):
+    try:
         analyze_dict = {} #몽고DB 저장 용 딕셔너리
         emotion_dict = {} #문장 별 감정 저장 용 딕셔터리
-        diary_sentences = text_keyword.split_sentences(diary_content)   #문장 별 분리
+        diary_sentences = text_keyword.split_sentences(entry.diary_content)   #문장 별 분리
         diary_noun_sentences = text_keyword.noun_sentences(diary_sentences) #키워드 추출 용 어간 추출
         for index, sentence in enumerate(diary_sentences):  
             emotion_dict[str(index)] = text_emotion.predict_emotion(sentence).tolist()  #감정 분석
-        analyze_dict['diary_index'] = diary_index
+        analyze_dict['diary_index'] = entry.diary_index
         analyze_dict['sentence'] = diary_sentences  #분리된 문장 리스트
         analyze_dict['emotion'] = emotion_dict  #문장 별 감정 수치
         analyze_dict['keyword'] = text_keyword.get_keyword(diary_noun_sentences)    #키워드는 어간 추출 리스트 기반

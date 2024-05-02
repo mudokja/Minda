fd0fc176a361b4a07b0f4c9294494136f30b8588
jiwon1119 <wldnjs5045@hanmail.net>
2024-05-02 16:13:47 +0900

    api_ui_

--- diary_fe/lib/src/screens/diary_list_page.dart
+++ diary_fe/lib/src/screens/diary_list_page.dart
@@ -140,122 +140,122 @@
 
 
 
-// // DiaryListPage
-// // class DiaryListPage extends StatefulWidget {
-// //   const DiaryListPage({super.key});
+// DiaryListPage
+// class DiaryListPage extends StatefulWidget {
+//   const DiaryListPage({super.key});
 
-// //   @override
-// //   _DiaryListPageState createState() => _DiaryListPageState();
-// // }
+//   @override
+//   _DiaryListPageState createState() => _DiaryListPageState();
+// }
 
-// // class _DiaryListPageState extends State<DiaryListPage> {
+// class _DiaryListPageState extends State<DiaryListPage> {
 
 
-// //   // 날짜색
-// //   // 날짜에 따른 MoodEntry를 가져오는 함수
-// //   MoodEntry getMoodEntryForDate(DateTime date) {
-// //     // 여기서는 가정하여 MoodEntry를 미리 생성해둔 것으로 가정합니다.
-// //     // 실제로는 날짜에 맞는 MoodEntry를 데이터베이스 등에서 가져와야 합니다.
-// //     return MoodEntry(
-// //       date: date,
-// //       diary_happiness: "기쁨", // 예시 감정
-// //     );
-// //   }
+//   // 날짜색
+//   // 날짜에 따른 MoodEntry를 가져오는 함수
+//   MoodEntry getMoodEntryForDate(DateTime date) {
+//     // 여기서는 가정하여 MoodEntry를 미리 생성해둔 것으로 가정합니다.
+//     // 실제로는 날짜에 맞는 MoodEntry를 데이터베이스 등에서 가져와야 합니다.
+//     return MoodEntry(
+//       date: date,
+//       diary_happiness: "기쁨", // 예시 감정
+//     );
+//   }
 
-// // // MoodEntry 모델에서 감정에 따른 색상을 가져오는 함수
-// //   Color getColorFromMood(String? mood) {
-// //     return MoodEntryHelper.getColorFromMood(mood);
-// //   }
+// // MoodEntry 모델에서 감정에 따른 색상을 가져오는 함수
+//   Color getColorFromMood(String? mood) {
+//     return MoodEntryHelper.getColorFromMood(mood);
+//   }
 
-// //   // 날짜색_
+//   // 날짜색_
 
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Scaffold(
-// //       appBar: AppBar(
-// //         centerTitle: true,
-// //         title: const Text(
-// //           '일기장',
-// //           style: TextStyle(
-// //             color: Colors.white,
-// //             fontWeight: FontWeight.bold,
-// //           ),
-// //         ),
-// //         backgroundColor: Colors.transparent,
-// //         elevation: 0.0,
-// //       ),
-// //       extendBodyBehindAppBar: true,
-// //       body: Stack(
-// //         children: <Widget>[
-// //           const Background(), // Background 위젯의 정확한 구현을 확인해주세요.
-// //           SafeArea(
-// //             child: Column(
-// //               children: <Widget>[
-// //                 Padding(
-// //                   padding: const EdgeInsets.symmetric(
-// //                       vertical: 0.0, horizontal: 20.0),
-// //                   child: Row(
-// //                     children: <Widget>[
-// //                       IconButton(
-// //                         onPressed: () {
-// //                           CalendarDialog.showCalendarDialog(context);
-// //                         },
-// //                         icon: Image.asset(
-// //                           'assets/icon/white_calendar.png',
-// //                           width: 45,
-// //                           height: 45,
-// //                         ),
-// //                       ),
-// //                       const SizedBox(width: 13),
-// //                       Expanded(
-// //                         child: TextField(
-// //                           style: const TextStyle(color: Colors.white),
-// //                           decoration: InputDecoration(
-// //                             hintText: '제목/내용',
-// //                             fillColor: Colors.transparent,
-// //                             filled: true,
-// //                             contentPadding:
-// //                                 const EdgeInsets.symmetric(vertical: 10.0)
-// //                                     .copyWith(left: 20.0),
-// //                             border: OutlineInputBorder(
-// //                               borderRadius: BorderRadius.circular(15.0),
-// //                               borderSide: BorderSide.none,
-// //                             ),
-// //                             enabledBorder: OutlineInputBorder(
-// //                               borderRadius: BorderRadius.circular(15.0),
-// //                               borderSide: const BorderSide(
-// //                                   color: Colors.white, width: 2.0),
-// //                             ),
-// //                             focusedBorder: OutlineInputBorder(
-// //                               borderRadius: BorderRadius.circular(15.0),
-// //                               borderSide: const BorderSide(
-// //                                   color: Colors.white, width: 3.0),
-// //                             ),
-// //                             hintStyle: const TextStyle(color: Colors.white),
-// //                             suffixIcon: const Icon(Icons.search_rounded,
-// //                                 color: Colors.white, size: 30),
-// //                           ),
-// //                         ),
-// //                       ),
-// //                     ],
-// //                   ),
-// //                 ),
-// //                 Expanded(
-// //                   child: Padding(
-// //                     padding: const EdgeInsets.symmetric(
-// //                         vertical: 20.0, horizontal: 5.0),
-// //                     child: _buildDiaryList(),
-// //                     // child: _buildDiaryListWithButtons(), // 변경된 함수 사용
-// //                   ),
-// //                 ),
-// //               ],
-// //             ),
-// //           ),
-// //         ],
-// //       ),
-// //     );
-// //   }
-// // }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: const Text(
+//           '일기장',
+//           style: TextStyle(
+//             color: Colors.white,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//         backgroundColor: Colors.transparent,
+//         elevation: 0.0,
+//       ),
+//       extendBodyBehindAppBar: true,
+//       body: Stack(
+//         children: <Widget>[
+//           const Background(), // Background 위젯의 정확한 구현을 확인해주세요.
+//           SafeArea(
+//             child: Column(
+//               children: <Widget>[
+//                 Padding(
+//                   padding: const EdgeInsets.symmetric(
+//                       vertical: 0.0, horizontal: 20.0),
+//                   child: Row(
+//                     children: <Widget>[
+//                       IconButton(
+//                         onPressed: () {
+//                           CalendarDialog.showCalendarDialog(context);
+//                         },
+//                         icon: Image.asset(
+//                           'assets/icon/white_calendar.png',
+//                           width: 45,
+//                           height: 45,
+//                         ),
+//                       ),
+//                       const SizedBox(width: 13),
+//                       Expanded(
+//                         child: TextField(
+//                           style: const TextStyle(color: Colors.white),
+//                           decoration: InputDecoration(
+//                             hintText: '제목/내용',
+//                             fillColor: Colors.transparent,
+//                             filled: true,
+//                             contentPadding:
+//                                 const EdgeInsets.symmetric(vertical: 10.0)
+//                                     .copyWith(left: 20.0),
+//                             border: OutlineInputBorder(
+//                               borderRadius: BorderRadius.circular(15.0),
+//                               borderSide: BorderSide.none,
+//                             ),
+//                             enabledBorder: OutlineInputBorder(
+//                               borderRadius: BorderRadius.circular(15.0),
+//                               borderSide: const BorderSide(
+//                                   color: Colors.white, width: 2.0),
+//                             ),
+//                             focusedBorder: OutlineInputBorder(
+//                               borderRadius: BorderRadius.circular(15.0),
+//                               borderSide: const BorderSide(
+//                                   color: Colors.white, width: 3.0),
+//                             ),
+//                             hintStyle: const TextStyle(color: Colors.white),
+//                             suffixIcon: const Icon(Icons.search_rounded,
+//                                 color: Colors.white, size: 30),
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//                 Expanded(
+//                   child: Padding(
+//                     padding: const EdgeInsets.symmetric(
+//                         vertical: 20.0, horizontal: 5.0),
+//                     child: _buildDiaryList(),
+//                     // child: _buildDiaryListWithButtons(), // 변경된 함수 사용
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 // // 가상의 DiaryPage 위젯
 // class DiaryPage extends StatelessWidget {
@@ -284,6 +284,7 @@ import 'package:provider/provider.dart';
 import 'package:diary_fe/src/models/DiaryEntry.dart';
 import 'package:diary_fe/src/services/diary_api_service.dart';
 import 'package:diary_fe/src/widgets/background.dart';
+import 'package:diary_fe/src/widgets/calendar_dialog.dart';
 
 // DiaryListPage 스테이트풀 위젯
 class DiaryListPage extends StatefulWidget {
@@ -319,6 +320,70 @@ class _DiaryListPageState extends State<DiaryListPage> {
     }
   }
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: const Text('일기장', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+//         backgroundColor: Colors.transparent,
+//         elevation: 0.0,
+//       ),
+//       extendBodyBehindAppBar: true,
+//       body: Stack(
+//         children: <Widget>[
+//           const Background(),
+//           SafeArea(
+//             child: isLoading
+//               ? const Center(child: CircularProgressIndicator())
+//               : diaryEntries.isEmpty
+//                 ? const Center(child: Text("No diary entries found"))
+//                 : ListView.builder(
+//                     itemCount: diaryEntries.length,
+//                     itemBuilder: (context, index) {
+//                       final entry = diaryEntries[index];
+//                       return ListTile(
+//                         title: Text(entry.diaryTitle ?? 'No Title'),
+//                         subtitle: Text("Set on ${entry.diarySetDate} - Happiness: ${entry.diaryHappiness}, Sadness: ${entry.diarySadness}"),
+//                       );
+//                     },
+//                   ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class DiaryListPage extends StatefulWidget {
+//   const DiaryListPage({Key? key}) : super(key: key);
+
+//   @override
+//   _DiaryListPageState createState() => _DiaryListPageState();
+// }
+
+// class _DiaryListPageState extends State<DiaryListPage> {
+//   bool isLoading = true; // 로딩 상태
+//   List<DiaryEntry> diaryEntries = []; // 일기 목록
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   fetchDiaryEntries(); // 일기 목록 가져오기
+  // }
+
+  void fetchDiaryEntries() async {
+    setState(() => isLoading = true);
+    try {
+      // 여기에 데이터 가져오는 로직 구현
+      // 예: diaryEntries = await DiaryApiService.getDiaryEntries();
+      setState(() => isLoading = false);
+    } catch (e) {
+      print('Error fetching diary entries: $e');
+      setState(() => isLoading = false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -331,22 +396,65 @@ class _DiaryListPageState extends State<DiaryListPage> {
       extendBodyBehindAppBar: true,
       body: Stack(
         children: <Widget>[
-          const Background(),
+          const Background(), // Background 위젯의 정확한 구현을 확인해주세요.
           SafeArea(
-            child: isLoading
-              ? const Center(child: CircularProgressIndicator())
-              : diaryEntries.isEmpty
-                ? const Center(child: Text("No diary entries found"))
-                : ListView.builder(
-                    itemCount: diaryEntries.length,
-                    itemBuilder: (context, index) {
-                      final entry = diaryEntries[index];
-                      return ListTile(
-                        title: Text(entry.diaryTitle ?? 'No Title'),
-                        subtitle: Text("Set on ${entry.diarySetDate} - Happiness: ${entry.diaryHappiness}, Sadness: ${entry.diarySadness}"),
-                      );
-                    },
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () => CalendarDialog.showCalendarDialog(context),
+                        icon: Image.asset('assets/icon/white_calendar.png', width: 45, height: 45),
+                      ),
+                      const SizedBox(width: 13),
+                      Expanded(
+                        child: TextField(
+                          style: const TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            hintText: '제목/내용',
+                            fillColor: Colors.transparent,
+                            filled: true,
+                            contentPadding: const EdgeInsets.symmetric(vertical: 10.0).copyWith(left: 20.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              borderSide: const BorderSide(color: Colors.white, width: 2.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              borderSide: const BorderSide(color: Colors.white, width: 3.0),
+                            ),
+                            hintStyle: const TextStyle(color: Colors.white),
+                            suffixIcon: const Icon(Icons.search_rounded, color: Colors.white, size: 30),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
+                ),
+                Expanded(
+                  child: isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : diaryEntries.isEmpty
+                      ? const Center(child: Text("No diary entries found"))
+                      : ListView.builder(
+                          itemCount: diaryEntries.length,
+                          itemBuilder: (context, index) {
+                            final entry = diaryEntries[index];
+                            return ListTile(
+                              title: Text(entry.diaryTitle ?? 'No Title'),
+                              subtitle: Text("Set on ${entry.diarySetDate} - Happiness: ${entry.diaryHappiness}, Sadness: ${entry.diarySadness}"),
+                            );
+                          },
+                        ),
+                ),
+              ],
+            ),
           ),
         ],
       ),

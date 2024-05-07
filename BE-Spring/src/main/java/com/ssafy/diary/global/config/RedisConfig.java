f4d8072d411e74cc6c253e6f1e3968ffc2fb1f68
mudokja <mudokja@gmail.com>
2024-05-07 10:06:40 +0900

    feat: redishash TTL 관련 설정 수정
    indexed 필드는 ttl 적용 안되는 문제 수정

--- BE-Spring/src/main/java/com/ssafy/diary/global/config/RedisConfig.java
+++ BE-Spring/src/main/java/com/ssafy/diary/global/config/RedisConfig.java
@@ -27,12 +27,14 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import java.time.Duration;
 
+import static org.springframework.data.redis.core.RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP;
+
 @Configuration
 @EnableConfigurationProperties({RedisProperties.class})
 @RequiredArgsConstructor
 @Slf4j
 @EnableCaching
-@EnableRedisRepositories()
+@EnableRedisRepositories(enableKeyspaceEvents = ON_STARTUP)
 public class RedisConfig {
 
     private final RedisProperties redisProperties;

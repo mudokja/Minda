56ead6172005a021083056601f2c501632140511
강보훈 <kbh10423@gmail.com>
2024-05-07 22:55:30 +0900

    feat :알림 수신 가능

--- diary_fe/lib/main.dart
+++ diary_fe/lib/main.dart
@@ -2,24 +2,54 @@ import 'package:diary_fe/firebase_options.dart';
 import 'package:diary_fe/src/screens/intro_page.dart';
 import 'package:diary_fe/src/screens/pages.dart';
 import 'package:diary_fe/src/services/user_provider.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
-import 'package:diary_fe/src/screens/diary_list_page.dart';
-import 'package:diary_fe/src/models/MoodEntry.dart'; // MoodEntry를 import 해야합니다.
+
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:provider/provider.dart';
 import 'package:diary_fe/src/services/services_initializer.dart'; // setup.dart 파일 import
-// main.dart 파일
-import 'package:flutter/material.dart';
-import 'package:diary_fe/src/services/services_initializer.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("백그라운드 메시지 처리.. ${message.notification!.body!}");
+}
+
+void initializeNotification() async {
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(const AndroidNotificationChannel(
+          'high_importance_channel', 'high_importance_notification',
+          importance: Importance.max));
+
+  await flutterLocalNotificationsPlugin.initialize(const InitializationSettings(
+    android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+  ));
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  initializeNotification();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseAnalytics.instance.logAppOpen();
 
   // runApp() 호출 전 Flutter SDK 초기화
   KakaoSdk.init(
@@ -47,9 +77,72 @@ class MyApp extends StatelessWidget {
         builder: (context, userProvider, child) {
           // 로그인 상태에 따라 다른 페이지 렌더링
           return Scaffold(
-            body: userProvider.isLoggedIn ? const Pages() : const IntroPage(),
+            body: userProvider.isLoggedIn ? const Pages() : const HomePage(),
           );
         },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var messageString = "";
+
+  void getMyDeviceToken() async {
+    final token = await FirebaseMessaging.instance.getToken();
+
+    print("내 디바이스 토큰: $token");
+  }
+
+  @override
+  void initState() {
+    getMyDeviceToken();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      RemoteNotification? notification = message.notification;
+
+      if (notification != null) {
+        FlutterLocalNotificationsPlugin().show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              'high_importance_channel',
+              'high_importance_notification',
+              importance: Importance.max,
+            ),
+          ),
+        );
+
+        setState(() {
+          messageString = message.notification!.body!;
+
+          print("Foreground 메시지 수신: $messageString");
+        });
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("메시지 내용: $messageString"),
+          ],
+        ),
       ),
     );
   }

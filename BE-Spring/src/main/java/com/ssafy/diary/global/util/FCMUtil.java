4f4b9ddd5d28e52e8f98407d9861f470f0681831
mudokja <mudokja@gmail.com>
2024-05-09 12:44:51 +0900

    fix: ttl 시간 설정 버그 수정

--- BE-Spring/src/main/java/com/ssafy/diary/global/util/FCMUtil.java
+++ BE-Spring/src/main/java/com/ssafy/diary/global/util/FCMUtil.java
@@ -10,6 +10,167 @@ import java.util.List;
 import java.util.Map;
 @Slf4j
 public class FCMUtil {
+    private static final long DEFAULT_ANDROID_TTL_TIME= 60L *1000*60*24*3;
+    public static void sendAllNotificationMessage(String title, String message, @Nullable Map<String,String> data, String topic, @Nullable Long androidTtlSec) throws FirebaseMessagingException {
+        Message requestMessage=Message.builder()
+                .setNotification(Notification.builder()
+                        .setBody(message)
+                        .setTitle(title)
+                        .build())
+                .putAllData(data)
+                .setAndroidConfig(AndroidConfig.builder()
+                        .setTtl(androidTtlSec==null?DEFAULT_ANDROID_TTL_TIME:androidTtlSec*1000)
+                        .build())
+                .setWebpushConfig(WebpushConfig.builder()
+                        .setNotification(WebpushNotification.builder()
+                                .setDirection(WebpushNotification.Direction.AUTO)
+                                .setRenotify(true)
+                                .setLanguage("ko-KR")
+                                .build())
+                        .build())
+                .setTopic(topic)
+                .build();
+
+        String response = FirebaseMessaging.getInstance()
+                .send(requestMessage);
+        showResponseLog(response);
+    }
+    public static void sendAllNotificationMessage(String title, String message, List<String> registrationToken, @Nullable Long androidTtlSec) throws FirebaseMessagingException {
+        List<Message> requestMessage=new ArrayList<>();
+        registrationToken.forEach(token->
+                requestMessage.add(Message.builder()
+                        .setNotification(Notification.builder()
+                                .setBody(message)
+                                .setTitle(title)
+                                .build())
+                        .setAndroidConfig(AndroidConfig.builder()
+                                .setTtl(androidTtlSec==null?DEFAULT_ANDROID_TTL_TIME:androidTtlSec*1000)
+                                .build())
+                        .setWebpushConfig(WebpushConfig.builder()
+                                .setNotification(WebpushNotification.builder()
+                                        .setDirection(WebpushNotification.Direction.AUTO)
+                                        .setRenotify(true)
+                                        .setLanguage("ko-KR")
+                                        .build())
+                                .build())
+                        .setToken(token)
+                        .build()
+                ));
+
+        BatchResponse response = FirebaseMessaging.getInstance()
+                .sendEach(requestMessage);
+        showBatchResponseLog(response);
+    }
+    public static void sendAllNotificationMessage(String title, String message, String imageUrl, List<String> registrationToken, @Nullable Long androidTtlSec) throws FirebaseMessagingException {
+        List<Message> requestMessage=new ArrayList<>();
+        registrationToken.forEach(token->
+                requestMessage.add(Message.builder()
+                        .setNotification(Notification.builder()
+                                .setBody(message)
+                                .setTitle(title)
+                                .setImage(imageUrl)
+                                .build())
+                        .setAndroidConfig(AndroidConfig.builder()
+                                .setTtl(androidTtlSec==null?DEFAULT_ANDROID_TTL_TIME:androidTtlSec*1000)
+                                .build())
+                        .setWebpushConfig(WebpushConfig.builder()
+                                .setNotification(WebpushNotification.builder()
+                                        .setDirection(WebpushNotification.Direction.AUTO)
+                                        .setRenotify(true)
+                                        .setLanguage("ko-KR")
+                                        .build())
+                                .build())
+                        .setToken(token)
+                        .build()
+                ));
+
+        BatchResponse response = FirebaseMessaging.getInstance()
+                .sendEach(requestMessage);
+        showBatchResponseLog(response);
+    }
+    public static void sendAllNotificationMessage(String title, String message, @Nullable Map<String,String> data, List<String> registrationToken, @Nullable Long androidTtlSec) throws FirebaseMessagingException {
+        List<Message> requestMessage=new ArrayList<>();
+        registrationToken.forEach(token->
+                requestMessage.add(Message.builder()
+                        .setNotification(Notification.builder()
+                                .setBody(message)
+                                .setTitle(title)
+                                .build())
+                        .putAllData(data)
+                        .setAndroidConfig(AndroidConfig.builder()
+                                .setTtl(androidTtlSec==null?DEFAULT_ANDROID_TTL_TIME:androidTtlSec*1000)
+                                .build())
+                        .setWebpushConfig(WebpushConfig.builder()
+                                .setNotification(WebpushNotification.builder()
+                                        .setDirection(WebpushNotification.Direction.AUTO)
+                                        .setRenotify(true)
+                                        .setLanguage("ko-KR")
+                                        .build())
+                                .build())
+                        .setToken(token)
+                        .build()
+                ));
+
+        BatchResponse response = FirebaseMessaging.getInstance()
+                .sendEach(requestMessage);
+        showBatchResponseLog(response);
+    }
+
+    private static void showBatchResponseLog(BatchResponse response) {
+        log.debug("Successfully sent notification message list : {}", response.getResponses());
+        log.debug("Failed sent notification message count : {}", response.getFailureCount());
+        log.debug("Successful sent notification message count : {}", response.getSuccessCount());
+    }
+
+    public static void sendNotificationMessage(String title, String message, @Nullable Map<String,String> data, String registrationToken, @Nullable Long androidTtlSec) throws FirebaseMessagingException {
+        Message requestMessage=Message.builder()
+                .setNotification(Notification.builder()
+                        .setBody(message)
+                        .setTitle(title)
+                        .build())
+                .putAllData(data)
+                .setAndroidConfig(AndroidConfig.builder()
+                        .setTtl(androidTtlSec==null?DEFAULT_ANDROID_TTL_TIME:androidTtlSec*1000)
+                        .build())
+                .setWebpushConfig(WebpushConfig.builder()
+                        .setNotification(WebpushNotification.builder()
+                                .setDirection(WebpushNotification.Direction.AUTO)
+                                .setRenotify(true)
+                                .setLanguage("ko-KR")
+                                .build())
+                        .build())
+                .setToken(registrationToken)
+                .build();
+
+        String response = FirebaseMessaging.getInstance()
+                .send(requestMessage);
+        showResponseLog(response);
+
+    }
+    public static void sendNotificationMessage(String title, String message, String registrationToken, @Nullable Long androidTtlSec) throws FirebaseMessagingException {
+        Message requestMessage=Message.builder()
+                .setNotification(Notification.builder()
+                        .setBody(message)
+                        .setTitle(title)
+                        .build())
+                .setAndroidConfig(AndroidConfig.builder()
+                        .setTtl(androidTtlSec==null ?DEFAULT_ANDROID_TTL_TIME:androidTtlSec*1000)
+                        .build())
+                .setWebpushConfig(WebpushConfig.builder()
+                        .setNotification(WebpushNotification.builder()
+                                .setDirection(WebpushNotification.Direction.AUTO)
+                                .setRenotify(true)
+                                .setLanguage("ko-KR")
+                                .build())
+                        .build())
+                .setToken(registrationToken)
+                .build();
+
+        String response = FirebaseMessaging.getInstance()
+                .send(requestMessage);
+        showResponseLog(response);
+
+    }
     public static void sendAllNotificationMessage(String title, String message, @Nullable Map<String,String> data, String topic) throws FirebaseMessagingException {
        Message requestMessage=Message.builder()
                         .setNotification(Notification.builder()
@@ -18,7 +179,7 @@ public class FCMUtil {
                                 .build())
                         .putAllData(data)
                         .setAndroidConfig(AndroidConfig.builder()
-                                .setTtl(new LocalDateTime().plusDays(7).getMillisOfSecond())
+                                .setTtl(DEFAULT_ANDROID_TTL_TIME)
                                 .build())
                         .setWebpushConfig(WebpushConfig.builder()
                                 .setNotification(WebpushNotification.builder()
@@ -32,8 +193,13 @@ public class FCMUtil {
 
         String response = FirebaseMessaging.getInstance()
                 .send(requestMessage);
-        log.debug("Successfully sent notification message : {}",response);
+        showResponseLog(response);
     }
+
+    private static void showResponseLog(String response) {
+        log.debug("Successfully sent notification message : {}", response);
+    }
+
     public static void sendAllNotificationMessage(String title, String message, List<String> registrationToken) throws FirebaseMessagingException {
         List<Message> requestMessage=new ArrayList<>();
         registrationToken.forEach(token->
@@ -43,7 +209,7 @@ public class FCMUtil {
                                 .setTitle(title)
                                 .build())
                         .setAndroidConfig(AndroidConfig.builder()
-                                .setTtl(new LocalDateTime().plusDays(7).getMillisOfSecond())
+                                .setTtl(DEFAULT_ANDROID_TTL_TIME)
                                 .build())
                         .setWebpushConfig(WebpushConfig.builder()
                                 .setNotification(WebpushNotification.builder()
@@ -58,9 +224,7 @@ public class FCMUtil {
 
         BatchResponse response = FirebaseMessaging.getInstance()
                 .sendEach(requestMessage);
-        log.debug("Successfully sent notification message list : {}",response.getResponses());
-        log.debug("Failed sent notification message count : {}",response.getFailureCount());
-        log.debug("Successful sent notification message count : {}",response.getSuccessCount());
+        showBatchResponseLog(response);
     }
     public static void sendAllNotificationMessage(String title, String message, String imageUrl, List<String> registrationToken) throws FirebaseMessagingException {
         List<Message> requestMessage=new ArrayList<>();
@@ -72,7 +236,7 @@ public class FCMUtil {
                                 .setImage(imageUrl)
                                 .build())
                         .setAndroidConfig(AndroidConfig.builder()
-                                .setTtl(new LocalDateTime().plusDays(7).getMillisOfSecond())
+                                .setTtl(DEFAULT_ANDROID_TTL_TIME)
                                 .build())
                         .setWebpushConfig(WebpushConfig.builder()
                                 .setNotification(WebpushNotification.builder()
@@ -87,9 +251,7 @@ public class FCMUtil {
 
         BatchResponse response = FirebaseMessaging.getInstance()
                 .sendEach(requestMessage);
-        log.debug("Successfully sent notification message list : {}",response.getResponses());
-        log.debug("Failed sent notification message count : {}",response.getFailureCount());
-        log.debug("Successful sent notification message count : {}",response.getSuccessCount());
+        showBatchResponseLog(response);
     }
     public static void sendAllNotificationMessage(String title, String message, @Nullable Map<String,String> data, List<String> registrationToken) throws FirebaseMessagingException {
         List<Message> requestMessage=new ArrayList<>();
@@ -101,7 +263,7 @@ public class FCMUtil {
                                         .build())
                                 .putAllData(data)
                                 .setAndroidConfig(AndroidConfig.builder()
-                                        .setTtl(new LocalDateTime().plusDays(7).getMillisOfSecond())
+                                        .setTtl(DEFAULT_ANDROID_TTL_TIME)
                                         .build())
                                 .setWebpushConfig(WebpushConfig.builder()
                                         .setNotification(WebpushNotification.builder()
@@ -116,9 +278,7 @@ public class FCMUtil {
 
         BatchResponse response = FirebaseMessaging.getInstance()
                 .sendEach(requestMessage);
-        log.debug("Successfully sent notification message list : {}",response.getResponses());
-        log.debug("Failed sent notification message count : {}",response.getFailureCount());
-        log.debug("Successful sent notification message count : {}",response.getSuccessCount());
+        showBatchResponseLog(response);
     }
     public static void sendNotificationMessage(String title, String message, @Nullable Map<String,String> data, String registrationToken) throws FirebaseMessagingException {
         Message requestMessage=Message.builder()
@@ -128,7 +288,7 @@ public class FCMUtil {
                                 .build())
                         .putAllData(data)
                         .setAndroidConfig(AndroidConfig.builder()
-                                .setTtl(new LocalDateTime().plusDays(7).getMillisOfSecond())
+                                .setTtl(DEFAULT_ANDROID_TTL_TIME)
                                 .build())
                         .setWebpushConfig(WebpushConfig.builder()
                                 .setNotification(WebpushNotification.builder()
@@ -142,7 +302,7 @@ public class FCMUtil {
 
         String response = FirebaseMessaging.getInstance()
                 .send(requestMessage);
-        log.debug("Successfully sent notification message : {}",response);
+        showResponseLog(response);
 
     }
     public static void sendNotificationMessage(String title, String message, String registrationToken) throws FirebaseMessagingException {
@@ -152,7 +312,7 @@ public class FCMUtil {
                         .setTitle(title)
                         .build())
                 .setAndroidConfig(AndroidConfig.builder()
-                        .setTtl(new LocalDateTime().plusDays(7).getMillisOfSecond())
+                        .setTtl(DEFAULT_ANDROID_TTL_TIME)
                         .build())
                 .setWebpushConfig(WebpushConfig.builder()
                         .setNotification(WebpushNotification.builder()
@@ -166,7 +326,7 @@ public class FCMUtil {
 
         String response = FirebaseMessaging.getInstance()
                 .send(requestMessage);
-        log.debug("Successfully sent notification message : {}",response);
+        showResponseLog(response);
 
     }
 }
